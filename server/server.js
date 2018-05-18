0cc79bf239ffa8657b2085b638d76b63ae750eb7
Hernan Yarbi <hernan.yarbi@gmail.com>
2018-05-18 14:38:49 -0300

    Server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,43 @@
 require('./config/config');
 
-let express = require('express')
-let app = express()
+let express = require('express');
+let app = express();
+const bodyParser = require('body-parser');
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/usuario', function (req, res) {
+  res.json('Get User');
+})
+ 
+app.post('/usuario', function (req, res) {
+  res.json({
+      person: req.body
+  })
+})
+ 
+app.put('/usuario/:id', function (req, res) {
+    if(req.params.id){
+        res.json({
+            id:req.params.id
+        })
+    }else{
+        res.status(400).json({
+            ok: false,
+            msg: 'debe ingresar un id'
+        })
+    }
+})
+ 
+app.delete('/usuario/:id', function (req, res) {
+    if(req.params.id){
+        res.json({
+            id:req.params.id
+        })
+    }else{
+        res.status(400).json({
+            ok: false,
+            msg: 'debe ingresar un id'
+        })
+    }
 })
  
 app.listen(process.env.PORT, () => {
