5cb9c0f56272cd6963b09e3d094b6e31ee06358f
hernan yarbi <hernan.yarbi@gmail.com>
2018-05-21 15:34:35 -0300

    addicion de publics

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
-
+const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('./routes/index'));
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 mongoose.connect(process.env.URLDB, (err, res) => {
     if(err) throw err;
