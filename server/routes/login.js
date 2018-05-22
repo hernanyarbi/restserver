5b6f9d0c8e31896e29d89a0d86a5703acc9730fa
hernan yarbi <hernan.yarbi@gmail.com>
2018-05-21 22:39:15 -0300

    Autenticacion con google termindad

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bcryp = require('bcrypt');
-const {OAuth2Client} = require('google-auth-library');
+const { OAuth2Client } = require('google-auth-library');
 const client = new OAuth2Client(process.env.CLIENT_ID);
 const jwt = require('jsonwebtoken');
 const Users = require('../models/user');
@@ -13,7 +13,7 @@ app.post('/login', (req, res) => {
     Users.findOne({ email: body.email }, (err, user) => {
 
         if (err) {
-            res.status500.json({
+            res.status(500).json({
                 ok: false,
                 err
             })
@@ -56,12 +56,81 @@ async function verify(token) {
     });
     const payload = ticket.getPayload();
     console.log(payload);
-  }
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture
+    }
+}
 
-app.post('/google', (req, res) => {
+app.post('/google', async (req, res) => {
 
     let token = req.body.idtoken;
-    verify(token);
+    let googleUser = await verify(token)
+        .catch(e => {
+            return res.status(403).json({
+                ok: false,
+                err: e
+            });
+        });
+
+    Users.findOne({ email: googleUser.email }, (err, user) => {
+        if (err) {
+            res.status(500).json({
+                ok: false,
+                err
+            })
+        } else if (user) {
+            if (user.email === googleUser.emial) {
+                res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Este correo ya esta en uso.'
+                    }
+                })
+            } else {
+                let token = jwt.sign({
+                    user
+                }, process.env.SEED, { expiresIn: process.env.CADUCATE_TOKEN })
+
+                res.json({
+                    ok: true,
+                    user,
+                    token
+                });
+            }
+
+        } else {
+            let user = new Users({
+                nombre: googleUser.nombre,
+                email: googleUser.email,
+                img: googleUser.img,
+                google: true,
+                password: 'google-auth'
+            });
+            user.save((err, user) => {
+                if (err) {
+                    res.status(500).json({
+                        ok: false,
+                        err
+                    })
+                }
+                let token = jwt.sign({
+                    user
+                }, process.env.SEED, { expiresIn: process.env.CADUCATE_TOKEN })
+
+                res.json({
+                    ok: true,
+                    user,
+                    token
+                });
+            });
+        }
+    });
+
+    // // // // // return res.json({
+    // // // // //     user: googleUser
+    // // // // // });
 
 });
 
